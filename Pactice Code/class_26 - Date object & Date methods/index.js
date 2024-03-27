40e41c9f2c7a701225bae49b391e0470d180e07b
Jahid Hossain <107812081+Jahid-Hossain75@users.noreply.github.com>
2024-03-27 17:21:38 +0600

    Add files via upload

---
+++ Pactice Code/class_26 - Date object & Date methods/index.js
@@ -0,0 +1,39 @@
+
+// Date Object & Date Methods
+
+var date = new Date();
+
+// year methods
+var current_year = date.getFullYear();
+document.write("Current Year = "+current_year+ "<br>");
+
+
+
+// month methods
+var current_month = date.getMonth();
+document.write("Current Month = "+current_month+ "<br>");
+
+
+// date methods
+var current_date = date.getDate();
+document.write("Current Date = "+current_date+ "<br>");
+
+
+// Hour methods
+var current_hour = date.getHours();
+document.write("Current Hour = "+current_hour+ "<br>");
+
+
+// Minute methods
+var current_minute = date.getMinutes();
+document.write("Current Minute = "+current_minute+ "<br>");
+
+
+// Second methods
+var current_second = date.getSeconds();
+document.write("Current Second = "+current_second+ "<br>");
+
+
+// Second ms
+var current_ms = date.getMilliseconds();
+document.write("Current MS = "+current_ms+ "<br>");
